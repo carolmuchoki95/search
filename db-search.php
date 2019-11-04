978711360385a8cb56800a66afb4d43d09248e5b
carolmuchoki95 <carolmuchoki95@gmail.com>
2019-11-04 10:54:33 +0300

    first commit with search functionality

---
+++ db-search.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<title>Search functionality in dynamic website using php, MySQL | Technopoints</title>
+<link rel="stylesheet" href="css/w3.css">
+</head>
+<body>
+<center>
+<div class="w3-panel w3-pink">
+  <h1 class="w3-opacity">
+  <b>Search Functionality using php, MySQL</b></h1>
+</div>
+
+<form action="php-database-search.php" method="post" class="w3-container">
+  <p>
+  <label>Search Box</label>
+  <input class="w3-input w3-animate-input" type="text" name="search" placeholder="Search name or company... " required="" style="width:30%"></p>
+  <button class="w3-btn w3-blue" type="submit" value="Submit">Search</button>
+  <p>
+  <div class="w3-container w3-blue">
+  <h4 style="text-shadow:1px 1px 0 #444">A tutorial on <a href="https://technopoints.co.in" target="new">Technopoints</a></h4></p>
+</div>
+</form>
+</center>
+</body>
+</html>
